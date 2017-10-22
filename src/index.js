167dcb51ab950628911287c118160443bec9fbbf
Andy Parker <andyparker84@gmail.com>
2017-10-22 13:55:52 -0500

    setup complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from "react";
 // bring in react dom to actually render it to the dom
 import ReactDom from "react-dom";
 // create a new component to produce some html
-const App = function() {
+const App = () => {
   return <div>Hi! </div>;
 };
 
 // take the components generate html and put in on the page
-ReactDom.render(<App />);
+ReactDom.render(<App />, document.querySelector(".container"));
