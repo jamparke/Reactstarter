c1d256807d1071f7b67944655e59f9dd3567a357
Andy Parker <andyparker84@gmail.com>
2017-10-22 14:55:36 -0500

    search bar installed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-//bring in react
+//bring in react from node modules
 import React from "react";
-// bring in react dom to actually render it to the dom
+// bring in react dom  from node modules to actually render it to the dom
 import ReactDom from "react-dom";
+
+// bring in individual components
+import SearchBar from "./components/search_bar";
+
+//Youtube api access key
+const API_KEY = "AIzaSyBDU9BwCNM05Uibq4FDr7jChHC0kHV72Dg";
+
 // create a new component to produce some html
 const App = () => {
-  return <div>Hi! </div>;
+  return <SearchBar />;
 };
 
 // take the components generate html and put in on the page
